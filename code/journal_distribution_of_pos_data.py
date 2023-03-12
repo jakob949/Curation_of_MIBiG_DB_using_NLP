b2636d8ee0a0a1c0b2ff119b9ee718c6f40ba85f
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-03-13 00:24:26 +0100

    Update

--- code/journal_distribution_of_pos_data.py
+++ code/journal_distribution_of_pos_data.py
@@ -38,40 +38,44 @@ def fetch_journal2(pubmed_id):
     print(title)
     return title
 
-dist = {}
-with open('creation_of_dataset/abstracts_good_unique_entries.pickle', 'rb') as file:
-    abstracts_good = pickle.load(file)
-    # Loop over the good_abstracts
-    for i, line in enumerate(abstracts_good):
-        # Add the journal to the distribution dictionary to count the number of occurrences
-        journal = fetch_journal2(abstracts_good[line][0])
-        if journal is not None:
-            dist[journal] = dist.get(journal, 0) + 1
-            # dist[fetch_journal2(abstracts_good[line][0])] = dist.get(fetch_journal2(abstracts_good[line][0]), 0) + 1
-
-    print(i)
-
-# Save the distribution list
-with open('creation_of_dataset/journal_distribution.pickle', 'wb') as file:
-    pickle.dump(dist, file)
+# dist = {}
+# with open('creation_of_dataset/abstracts_good_unique_entries.pickle', 'rb') as file:
+#     abstracts_good = pickle.load(file)
+#     # Loop over the good_abstracts
+#     for i, line in enumerate(abstracts_good):
+#         # Add the journal to the distribution dictionary to count the number of occurrences
+#         journal = fetch_journal2(abstracts_good[line][0])
+#         if journal is not None:
+#             dist[journal] = dist.get(journal, 0) + 1
+#             # dist[fetch_journal2(abstracts_good[line][0])] = dist.get(fetch_journal2(abstracts_good[line][0]), 0) + 1
+#
+# # Save the distribution list
+# with open('creation_of_dataset/journal_distribution.pickle', 'wb') as file:
+#     pickle.dump(dist, file)
 
 
 with open('creation_of_dataset/journal_distribution.pickle', 'rb') as file:
     dist = pickle.load(file)
 
 
-# sort the dictionary by value
+# # sort the dictionary by value
 dist = {k: v for k, v in sorted(dist.items(), key=lambda item: item[1], reverse=True)}
 
-# remove entries with less than 5 publications
-dist = {k: v for k, v in dist.items() if v >= 5}
+for line in dist:
+    print(line, dist[line])
+    input('Press enter to continue')
 
-# plot the distribution
-plt.bar(range(len(dist)), list(dist.values()), align='center')
-plt.xticks(range(len(dist)), list(dist.keys()), rotation=90, fontsize=5)
-plt.xlabel('Journals', fontsize=7)
-plt.ylabel('Number of publications', fontsize=7)
-plt.title('Distribution of journals', fontsize=10)
-plt.tight_layout()
-plt.savefig('distribution.png', bbox_inches='tight', dpi=300)
+
+# # remove entries with less than 5 publications
+# dist = {k: v for k, v in dist.items() if v >= 5}
+#
+# # plot the distribution
+# plt.bar(range(len(dist)), list(dist.values()), align='center')
+# plt.xticks(range(0, len(dist), 2), list(dist.keys())[0::2], rotation=90, fontsize=4)
+# plt.xlabel('Journals', fontsize=10)
+# plt.ylabel('Number of publications', fontsize=10)
+# plt.title('Distribution of journals', fontsize=12)
+# plt.tight_layout()
+# plt.savefig('distribution.png', bbox_inches='tight', dpi=300)
+
 
