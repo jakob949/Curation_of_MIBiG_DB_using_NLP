61ba12aa3b21d01d7499404ced0e09c5362ae12a
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-03-14 22:26:04 +0100

    Argsparse

--- code/nlp_04032023.py
+++ code/nlp_04032023.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import torch
 from torch.utils.data import Dataset, DataLoader
 from transformers import AutoModel, AutoTokenizer, AutoModelForMaskedLM, RobertaForSequenceClassification, RobertaTokenizer, BertTokenizer, BertForSequenceClassification, AdamW
 import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--logfile', type=str, help='name of the log file')
+parser.add_argument('-tr', '--trainfile', type=str, help='name of the training file')
+parser.add_argument('-te', '--testfile', type=str, help='name of the test file')
+args = parser.parse_args()
 
 # Define the dataset
 class Dataset(Dataset):
@@ -33,7 +39,7 @@ model = RobertaForSequenceClassification.from_pretrained('finetuned_model_robert
 tokenizer = RobertaTokenizer.from_pretrained('finetuned_model_roberta_4')
 
 # Define the dataloader
-file_paths = ["train_fold_1.txt"]
+file_paths = [args.trainfile]
 dataset = Dataset(file_paths)
 dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
 
@@ -44,7 +50,7 @@ save_model = False
 model.train()
 num_of_epochs = 4
 optimizer = AdamW(model.parameters(), lr=1e-5) # weight_decay=0.01
-with open('log_file_fold_1.txt', 'w') as f:
+with open(args.logfile, 'w') as f:
     print(f"Training for {num_of_epochs} epochs", file=f)
     print(f"Training for {num_of_epochs} epochs")
     for epoch in range(num_of_epochs):
@@ -62,7 +68,7 @@ with open('log_file_fold_1.txt', 'w') as f:
             optimizer.zero_grad()
 
     # Define the test dataloader, re-using
-    test_file_path = ["test_fold_1.txt"]
+    test_file_path = [args.testfile]
     test_dataset = Dataset(test_file_path)
     test_dataloader = DataLoader(test_dataset, batch_size=1)
 
@@ -75,20 +81,4 @@ with open('log_file_fold_1.txt', 'w') as f:
             print(f"Batch {i+1}/{len(test_dataloader)}", file=f)
 
             abstract_text, labels = batch
-            inputs = tokenizer(abstract_text, padding=True, truncation=True, return_tensors="pt")
-            outputs = model(inputs["input_ids"], inputs["attention_mask"])
-            predictions = torch.argmax(outputs.logits, dim=1)
-            print('Prediction class:', predictions.item(), '\tCorrect label:', labels.item(), '\tprobs',torch.nn.functional.softmax(outputs.logits, dim=1).tolist()[0], file=f)
-            total_correct_preds += torch.sum(predictions == labels).item()
-            total_samples += 1
-
-    accuracy = total_correct_preds / total_samples
-    print("Accuracy: {:.2f}%".format(accuracy * 100), file=f)
-
-# Save the fine-tuned model
-if save_model:
-  model_dir = f"finetuned_model_roberta_{num_of_epochs}"
-  model.save_pretrained(model_dir)
-  tokenizer.save_pretrained(model_dir)
-time_end = time.time()
-print(f"Time elapsed in this session: {round(time_end - time_start, 2)/ 60} minutes", file=f)
+            inputs = tokenizer(abstract_text, padding=True, truncation=True, return_tensors="
