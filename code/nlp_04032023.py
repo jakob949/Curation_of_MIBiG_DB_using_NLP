265ec1730369f3d40efa7e25693ecd5755c6d554
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-03-15 09:17:16 +0100

    fixing

--- code/nlp_04032023.py
+++ code/nlp_04032023.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import torch
 from torch.utils.data import Dataset, DataLoader
 from transformers import AutoModel, AutoTokenizer, AutoModelForMaskedLM, RobertaForSequenceClassification, RobertaTokenizer, BertTokenizer, BertForSequenceClassification, AdamW
@@ -79,6 +80,20 @@ with open(args.logfile, 'w') as f:
     with torch.no_grad():
         for i, batch in enumerate(test_dataloader):
             print(f"Batch {i+1}/{len(test_dataloader)}", file=f)
+            inputs = tokenizer(abstract_text, padding=True, truncation=True, return_tensors="pt")
+            outputs = model(inputs["input_ids"], inputs["attention_mask"])
+            predictions = torch.argmax(outputs.logits, dim=1)
+            print('Prediction class:', predictions.item(), '\tCorrect label:', labels.item(), '\tprobs',torch.nn.functional.softmax(outputs.logits, dim=1).tolist()[0], file=f)
+            total_correct_preds += torch.sum(predictions == labels).item()
+            total_samples += 1
 
-            abstract_text, labels = batch
-            inputs = tokenizer(abstract_text, padding=True, truncation=True, return_tensors="
+    accuracy = total_correct_preds / total_samples
+    print("Accuracy: {:.2f}%".format(accuracy * 100), file=f)
+
+# Save the fine-tuned model
+if save_model:
+  model_dir = f"finetuned_model_roberta_{num_of_epochs}"
+  model.save_pretrained(model_dir)
+  tokenizer.save_pretrained(model_dir)
+time_end = time.time()
+print(f"Time elapsed in this session: {round(time_end - time_start, 2)/ 60} minutes", file=f)
