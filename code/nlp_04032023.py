b8d68d63121a69afcff61eb5036526c29a3275eb
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-03-24 13:45:39 +0100

    biogpt

--- code/nlp_04032023.py
+++ code/nlp_04032023.py
@@ -1,27 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import torch
-import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-from transformers import AutoModel, AutoTokenizer
-from torch.optim import AdamW
+from transformers import AutoModel, AutoTokenizer, AutoModelForMaskedLM, RobertaForSequenceClassification, RobertaTokenizer, BertTokenizer, BertForSequenceClassification, AdamW
+import time
+import argparse
 
-class ClassificationModel(nn.Module):
-    def __init__(self, base_model, num_labels):
-        super(ClassificationModel, self).__init__()
-        self.base_model = base_model
-        self.num_labels = num_labels
-        self.classifier = nn.Linear(base_model.config.hidden_size, num_labels)
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--logfile', type=str, help='name of the log file')
+parser.add_argument('-tr', '--trainfile', type=str, help='name of the training file')
+parser.add_argument('-te', '--testfile', type=str, help='name of the test file')
+args = parser.parse_args()
 
-    def forward(self, input_ids, attention_mask, labels=None):
-        outputs = self.base_model(input_ids=input_ids, attention_mask=attention_mask)
-        logits = self.classifier(outputs.last_hidden_state[:, 0, :])
-        if labels is not None:
-            loss_fct = nn.CrossEntropyLoss()
-            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
-            return loss, logits
-        else:
-            return logits
-
-class TextClassificationDataset(Dataset):
+# Define the dataset
+class Dataset(Dataset):
     def __init__(self, file_paths):
         self.data = []
         for file_path in file_paths:
@@ -36,52 +29,75 @@ class TextClassificationDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
+time_start = time.time()
+
 # Load the pre-trained model
-base_model = AutoModel.from_pretrained("microsoft/biogpt")
-tokenizer = AutoTokenizer.from_pretrained("microsoft/biogpt")
-num_labels = 2
-model = ClassificationModel(base_model, num_labels)
+# model = RobertaForSequenceClassification.from_pretrained("allenai/biomed_roberta_base")
+# tokenizer = RobertaTokenizer.from_pretrained("allenai/biomed_roberta_base")
+
+# Loading pre-trained-fine-tuned model
+# model = RobertaForSequenceClassification.from_pretrained('finetuned_model_roberta_4')
+# tokenizer = RobertaTokenizer.from_pretrained('finetuned_model_roberta_4')
+
+model = RobertaForSequenceClassification.from_pretrained("roberta-base")
+tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
 
 # Define the dataloader
-file_paths = ["spacy_neg.txt"]
-dataset = TextClassificationDataset(file_paths)
-dataloader = DataLoader(dataset, batch_size=3, shuffle=True)
+file_paths = [args.trainfile]
+dataset = Dataset(file_paths)
+dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
 
-# Fine-tune the model
+
+
+## Fine-tune the model ##
+save_model = False
 model.train()
 num_of_epochs = 1
-optimizer = AdamW(model.parameters(), lr=1e-5)
-for epoch in range(num_of_epochs):
-    print(f"Epoch {epoch + 1}/{num_of_epochs}")
-    for i, batch in enumerate(dataloader):
-        print(f"Batch {i + 1}/{len(dataloader)}")
-        texts, labels = batch
-        inputs = tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
-        loss, logits = model(input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"], labels=labels)
+optimizer = AdamW(model.parameters(), lr=1e-5) # weight_decay=0.01
+with open(args.logfile, 'w') as f:
+    print(f"Training for {num_of_epochs} epochs", file=f)
+    print(f"Training for {num_of_epochs} epochs")
+    for epoch in range(num_of_epochs):
+        print(f"Epoch {epoch+1}/{num_of_epochs}", file=f)
+        for i, batch in enumerate(dataloader):
+            print(f"Batch {i+1}/{len(dataloader)}", file=f)
+            texts, labels = batch
+            inputs = tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
+            outputs = model(inputs["input_ids"], inputs["attention_mask"], labels=labels)
 
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+            loss = outputs.loss
+            loss.backward()
 
-# Define the test dataloader
-test_file_path = ["spacy_neg.txt"]
-test_dataset = TextClassificationDataset(test_file_path)
-test_dataloader = DataLoader(test_dataset, batch_size=1)
+            optimizer.step()
+            optimizer.zero_grad()
 
-# Evaluate the model on the test dataset
-model.eval()
-total_correct_preds = 0
-total_samples = 0
-with torch.no_grad():
-    for i, batch in enumerate(test_dataloader):
-        print(f"Batch {i + 1}/{len(test_dataloader)}")
-        abstract_text, labels = batch
-        inputs = tokenizer(abstract_text, padding=True, truncation=True, return_tensors="pt")
-        outputs = model(inputs["input_ids"], inputs["attention_mask"])
-        predictions = torch.argmax(outputs, dim=1)
-        print(f"Prediction class: {predictions.item()}\tCorrect label: {labels.item()}\tprobs {torch.nn.functional.softmax(outputs, dim=1).tolist()[0]}")
-        total_correct_preds += torch.sum(predictions == labels).item()
-        total_samples += 1
+    # Define the test dataloader, re-using
+    test_file_path = [args.testfile]
+    test_dataset = Dataset(test_file_path)
+    test_dataloader = DataLoader(test_dataset, batch_size=1)
 
-accuracy = total_correct_preds / total_samples
-print(f"Accuracy: {accuracy
+    # Evaluate the model on the test dataset
+    model.eval()
+    total_correct_preds = 0
+    total_samples = 0
+    with torch.no_grad():
+        for i, batch in enumerate(test_dataloader):
+            print(f"Batch {i+1}/{len(test_dataloader)}", file=f)
+            abstract_text, labels = batch
+            inputs = tokenizer(abstract_text, padding=True, truncation=True, return_tensors="pt")
+            outputs = model(inputs["input_ids"], inputs["attention_mask"])
+            predictions = torch.argmax(outputs.logits, dim=1)
+            print('Prediction class:', predictions.item(), '\tCorrect label:', labels.item(), '\tprobs',torch.nn.functional.softmax(outputs.logits, dim=1).tolist()[0], file=f)
+            total_correct_preds += torch.sum(predictions == labels).item()
+            total_samples += 1
+
+    accuracy = total_correct_preds / total_samples
+    print("Accuracy: {:.2f}%".format(accuracy * 100), file=f)
+
+# Save the fine-tuned model
+if save_model:
+  model_dir = f"finetuned_model_roberta_{num_of_epochs}"
+  model.save_pretrained(model_dir)
+  tokenizer.save_pretrained(model_dir)
+time_end = time.time()
+print(f"Time elapsed in this session: {round(time_end - time_start, 2)/ 60} minutes", file=f)
