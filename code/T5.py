dd92090c8825460344d47a4dc6796bb3d82bb992
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-04-11 14:33:58 +0200

    larger models

--- code/T5.py
+++ code/T5.py
@@ -51,7 +51,7 @@ test_dataset = Dataset(args.testfile, tokenizer)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-batch_size = 4
+batch_size = 1
 epochs = 25
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
