6e974e5b3f5e2e25a9f96dc09fc142305a66254c
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-04-28 10:20:48 +0200

    Peptide CV

--- code/CV_file_creation.py
+++ code/CV_file_creation.py
@@ -1,24 +1,43 @@
 import random
 import numpy as np
+import os
+
+def read_in_chunks(file_path):
+    with open(file_path, 'r') as f:
+        for line in f:
+            yield line
+
+def write_to_file(file_name, data):
+    with open(file_name, 'w') as f:
+        for line in data:
+            f.write(line)
+
+def combine_data(file_paths):
+    for file_path in file_paths:
+        for line in read_in_chunks(file_path):
+            yield line
+
+def split_into_folds(data, k):
+    folds = [list() for _ in range(k)]
+    i = 0
+    for line in data:
+        folds[i].append(line)
+        i = (i + 1) % k
+    return folds
+# dataset_protein_text_v2_shorten
+data = read_in_chunks('dataset_protein_peptides_complete.txt')
 # Read in the data from the two labeled files
-with open('dataset_negatives_titles_abstracts.txt', 'r') as f:
-    data_0 = f.readlines()
-with open('dataset_positives_titles_abstracts.txt', 'r') as f:
-    data_1 = f.readlines()
-
-data = data_0 + data_1
-
-with open ('dataset_protien_chem.txt', 'r') as f:
-    data = f.readlines()
+random.seed(1)
+# Shuffle the data
+shuffled_data = []
+for line in data:
+    shuffled_data.insert(random.randrange(len(shuffled_data) + 1), line)
 
 # Set the number of folds
 k = 5
 
-# shuffle the data
-random.shuffle(data)
-
 # Split the data into k folds
-folds = np.array_split(data, k)
+folds = split_into_folds(shuffled_data, k)
 
 # Loop over each fold and create test and train sets
 for i in range(k):
@@ -27,10 +46,13 @@ for i in range(k):
 
     # Get the training data for this fold
     train_folds = [f for j, f in enumerate(folds) if j != i]
-    train_data = np.concatenate(train_folds)
+    train_data = []
+    for fold in train_folds:
+        train_data.extend(fold)
 
     # Save the test and train data to text files
-    with open(f'test_fold_protein_{i}.txt', 'w') as f:
-        f.writelines(test_data)
-    with open(f'train_fold_protein_{i}.txt', 'w') as f:
-        f.writelines(train_data)
+    test_file_name = f'test_dataset_protein_v2_{i}.txt'
+    train_file_name = f'train_dataset_protein_v2_{i}.txt'
+
+    write_to_file(test_file_name, test_data)
+    write_to_file(train_file_name, train_data)
