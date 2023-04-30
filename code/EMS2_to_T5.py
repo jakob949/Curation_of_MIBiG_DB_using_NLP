241c292e5e2b369bfe3db1369aab8fca1d7b6daf
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-04-30 18:42:42 +0200

    increased model

--- code/EMS2_to_T5.py
+++ code/EMS2_to_T5.py
@@ -5,7 +5,7 @@ from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer, Auto
 from torchmetrics.text.rouge import ROUGEScore
 
 class ProteinDataset(torch.utils.data.Dataset):
-    def __init__(self, file_path, T5_tokenizer, esm_tokenizer, max_length=900):
+    def __init__(self, file_path, T5_tokenizer, esm_tokenizer, max_length=850):
         self.file_path = file_path
         self.data = self.load_data()
         self.T5_tokenizer = T5_tokenizer
@@ -21,7 +21,7 @@ class ProteinDataset(torch.utils.data.Dataset):
                 text_list = text.split('_')
 
                 # Check if any element in text_list is longer than 2000 characters
-                if all(len(element) <= 900 for element in text_list):
+                if all(len(element) <= 850 for element in text_list):
                     data.append((text_list, label))
         print(len(data))
         return data
