1f4262cb0937239dabc568e599a2ff0f57f24ae7
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-04-30 11:58:14 +0200

    increased model

--- code/EMS2_to_T5.py
+++ code/EMS2_to_T5.py
@@ -5,7 +5,7 @@ from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer, Auto
 from torchmetrics.text.rouge import ROUGEScore
 
 class ProteinDataset(torch.utils.data.Dataset):
-    def __init__(self, file_path, T5_tokenizer, esm_tokenizer, max_length=1750):
+    def __init__(self, file_path, T5_tokenizer, esm_tokenizer, max_length=1350):
         self.file_path = file_path
         self.data = self.load_data()
         self.T5_tokenizer = T5_tokenizer
@@ -21,7 +21,7 @@ class ProteinDataset(torch.utils.data.Dataset):
                 text_list = text.split('_')
 
                 # Check if any element in text_list is longer than 2000 characters
-                if all(len(element) <= 1750 for element in text_list):
+                if all(len(element) <= 1350 for element in text_list):
                     data.append((text_list, label))
         print(len(data))
         return data
@@ -88,7 +88,7 @@ def concat_seqs(text):
 num_epochs = 12
 learning_rate = 5e-5
 
-T5_model_name = 'google/flan-t5-small'
+T5_model_name = 'google/flan-t5-base'
 t5_tokenizer = T5Tokenizer.from_pretrained(T5_model_name)
 t5_config = T5Config.from_pretrained(T5_model_name)
 t5_model = T5ForConditionalGeneration.from_pretrained(T5_model_name, config=t5_config)
