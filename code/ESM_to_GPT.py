375a0cbc56a900185e0efa2baf0d08a0f92b70d7
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-05-03 14:46:53 +0200

    new model

--- code/ESM_to_GPT.py
+++ code/ESM_to_GPT.py
@@ -1,18 +1,14 @@
 from torch.utils.data import DataLoader
 import torch
 from torch import nn
-from transformers import GPT2Config, GPT2LMHeadModel, GPT2Tokenizer, AutoTokenizer, AutoModel, AdamW
+from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer, AutoTokenizer, AutoModel, AdamW
 from torchmetrics.text.rouge import ROUGEScore
-import os
-
-os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
-
 
 class ProteinDataset(torch.utils.data.Dataset):
-    def __init__(self, file_path, gpt2_tokenizer, esm_tokenizer, max_length=850):
+    def __init__(self, file_path, T5_tokenizer, esm_tokenizer, max_length=850):
         self.file_path = file_path
         self.data = self.load_data()
-        self.gpt2_tokenizer = gpt2_tokenizer
+        self.T5_tokenizer = T5_tokenizer
         self.esm_tokenizer = esm_tokenizer
         self.max_length = max_length
 
@@ -24,21 +20,22 @@ class ProteinDataset(torch.utils.data.Dataset):
                 label = line.split('\t')[2].strip('\n')
                 text_list = text.split('_')
 
+                # Check if any element in text_list is longer than 2000 characters
                 if all(len(element) <= 850 for element in text_list):
                     data.append((text_list, label))
         print(len(data))
         return data
+
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         text, label = self.data[idx]
-        input_encoding = self.gpt2_tokenizer(text, return_tensors="pt", max_length=self.max_length,
-                                             padding="max_length",
-                                             truncation=True)
-        target_encoding = self.gpt2_tokenizer(label, return_tensors="pt", max_length=200, padding="max_length",
-                                              truncation=True)
+        input_encoding = self.T5_tokenizer(text, return_tensors="pt", max_length=self.max_length, padding="max_length",
+                                           truncation=True)
+        target_encoding = self.T5_tokenizer(label, return_tensors="pt", max_length=200, padding="max_length",
+                                            truncation=True)
 
         return {
             "input_ids": input_encoding["input_ids"].squeeze(),
@@ -48,11 +45,11 @@ class ProteinDataset(torch.utils.data.Dataset):
             "label": label
         }
 
-
+# Function to process input sequences with the ESM2 model and return hidden states
 def get_esm_hidden_states(input_text):
     esm_input_tokens = esm_tokenizer(input_text, return_tensors='pt', padding=True, truncation=True)
-    esm_input_ids = esm_input_tokens['input_ids'].to(device)
-    esm_attention_mask = esm_input_tokens['attention_mask'].to(device)
+    esm_input_ids = esm_input_tokens['input_ids'].to(device)  # Move to device
+    esm_attention_mask = esm_input_tokens['attention_mask'].to(device)  # Move to device
     esm_outputs = esm_model(input_ids=esm_input_ids, attention_mask=esm_attention_mask)
     return esm_outputs[0]
 
@@ -64,7 +61,7 @@ def pad_to_match(tensor1, tensor2, dim):
     diff = size1 - size2
 
     if diff > 0:
-        padding = (0, 0, 0, diff)
+        padding = (0, 0, 0, diff)  # (left, right, top, bottom) padding
         tensor2 = nn.functional.pad(tensor2, padding)
     elif diff < 0:
         padding = (0, 0, 0, -diff)
@@ -72,73 +69,55 @@ def pad_to_match(tensor1, tensor2, dim):
 
     return tensor1, tensor2
 
-
 def concat_seqs(text):
     concat_hidden_states = None
     hidden_states_list = []
-
+    # Pad all items to the maximum length and concatenate
     for item in text:
+        # gets the ESM encoding/hidden_states for each protein sequence in the input list 'text'
         hidden_states_list.append(get_esm_hidden_states(item))
 
     max_length = max(item_hidden_states.size(1) for item_hidden_states in hidden_states_list)
 
-    padded_hidden_states_list = [
-        pad_to_match(item_hidden_states, torch.zeros(1, max_length, item_hidden_states.size(2)), dim=1)[0] for
-        item_hidden_states in hidden_states_list]
+    padded_hidden_states_list = [pad_to_match(item_hidden_states, torch.zeros(1, max_length, item_hidden_states.size(2)), dim=1)[0] for item_hidden_states in hidden_states_list]
     concat_hidden_states = torch.cat(padded_hidden_states_list, dim=1)
     return concat_hidden_states
 
 
-def resize_position_embeddings(model, new_max_position_embeddings):
-    old_position_embeddings = model.transformer.wpe.weight.data
-    new_position_embeddings = torch.zeros(new_max_position_embeddings, model.config.n_embd)
-    new_position_embeddings[: model.config.max_position_embeddings] = old_position_embeddings
-    model.transformer.wpe.weight.data = new_position_embeddings
-    model.config.max_position_embeddings = new_max_position_embeddings
+# Set up the training parameters
+num_epochs = 12
+learning_rate = 5e-5
 
-
-GPT2_model_name = 'gpt2'
-gpt2_tokenizer = GPT2Tokenizer.from_pretrained(GPT2_model_name)
-gpt2_config = GPT2Config.from_pretrained(GPT2_model_name)
-gpt2_model = GPT2LMHeadModel.from_pretrained(GPT2_model_name, config=gpt2_config)
-gpt2_tokenizer.pad_token = gpt2_tokenizer.eos_token
+T5_model_name = 'google/flan-t5-base'
+t5_tokenizer = T5Tokenizer.from_pretrained(T5_model_name)
+t5_config = T5Config.from_pretrained(T5_model_name)
+t5_model = T5ForConditionalGeneration.from_pretrained(T5_model_name, config=t5_config)
 
 esm_model_name = "facebook/esm2_t6_8M_UR50D"
 esm_tokenizer = AutoTokenizer.from_pretrained(esm_model_name)
 esm_model = AutoModel.from_pretrained(esm_model_name)
 
-projection = nn.Linear(esm_model.config.hidden_size, gpt2_config.n_embd)
+projection = nn.Linear(esm_model.config.hidden_size, t5_config.d_model)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = 'cpu'
-gpt2_model.to(device)
-gpt2_model.transformer.wpe.weight = nn.Parameter(gpt2_model.transformer.wpe.weight.data.to(device))
 
+t5_model.to(device)
 esm_model.to(device)
 projection.to(device)
 print(device)
-learning_rate = 5e-5
-new_max_position_embeddings = 1024
-resize_position_embeddings(gpt2_model, new_max_position_embeddings)
-
-train_dataset = ProteinDataset("train_dataset_protein_v2_0.txt",
-                               gpt2_tokenizer, esm_tokenizer)
-test_dataset = ProteinDataset("test_dataset_protein_v2_0.txt", gpt2_tokenizer,
-                              esm_tokenizer)
+train_dataset = ProteinDataset("train_dataset_protein_v2_0.txt", t5_tokenizer, esm_tokenizer)
+test_dataset = ProteinDataset("test_dataset_protein_v2_0.txt", t5_tokenizer, esm_tokenizer)
 
 train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
-optimizer = AdamW(list(gpt2_model.parameters()) + list(esm_model.parameters()) + list(projection.parameters()),
-                  lr=learning_rate)
+optimizer = AdamW(list(t5_model.parameters()) + list(esm_model.parameters()) + list(projection.parameters()), lr=learning_rate)
 
 rouge = ROUGEScore()
 
-num_epochs = 12
-
-
+# Training loop
 for epoch in range(num_epochs):
-    gpt2_model.train()
+    t5_model.train()
     esm_model.train()
     projection.train()
 
@@ -146,46 +125,75 @@ for epoch in range(num_epochs):
     num_train_batches = 0
 
     for batch in train_loader:
+        # Should be fixed - This only works for batch size 1...
+        #
         num_train_batches += 1
 
         text = batch["text_list"]
         labels = batch["labels"].to(device)
 
         concat_hidden_states = concat_seqs(text)
-        projected_hidden_states = projection(concat_hidden_states)
 
+        projected_hidden_states = projection(concat_hidden_states)
         optimizer.zero_grad()
 
-        input_embeddings = projected_hidden_states.to(device)
+        decoder_input_ids = torch.cat((torch.full((labels.size(0), 1), 0, dtype=torch.long, device=device), labels[:, :-1]), dim=-1)
 
-        bos_embedding = gpt2_model.transformer.wte(
-            torch.tensor([[gpt2_tokenizer.bos_token_id]], device=device).to(device))
-
-        input_embeddings = torch.cat([bos_embedding, input_embeddings], dim=1)
-
-        # Generate position_ids explicitly
-        position_ids = torch.arange(0, input_embeddings.size(1), dtype=torch.long,
-                                    device=input_embeddings.device).unsqueeze(0).to(device)
-
-        gpt2_outputs = gpt2_model(
-            inputs_embeds=input_embeddings,
-            past_key_values=None,
+        t5_outputs = t5_model(
+            input_ids=None,
+            attention_mask=None,
+            decoder_input_ids=None,
+            encoder_outputs=(projected_hidden_states, None),
             labels=labels,
-            position_ids=position_ids,  # Pass the position_ids explicitly
         )
 
         with torch.no_grad():
-            train_predicted_labels = gpt2_tokenizer.decode(gpt2_outputs.logits[0].argmax(dim=-1).tolist(),
-                                                           skip_special_tokens=True)
+            train_predicted_labels = t5_tokenizer.decode(t5_outputs.logits[0].argmax(dim=-1).tolist(), skip_special_tokens=True, num_of_beams=5)
             train_true_labels = [batch["label"][0]]
             train_rouge_score = rouge(train_predicted_labels, train_true_labels)["rouge1_fmeasure"]
             rouge_train_accumulated += train_rouge_score
+            #print(f"train_rouge_score: {train_rouge_score}")
+            #print(f"train_true_labels: {train_true_labels},train_predicted_labels: {train_predicted_labels} ")
 
-        loss = gpt2_outputs.loss
+        loss = t5_outputs.loss
         loss.backward()
         optimizer.step()
 
-    # Calculate and print the average training Rouge score for the epoch
-    rouge_train_average = rouge_train_accumulated / num_train_batches
-    print(f"Epoch {epoch + 1}/{num_epochs}, Training Rouge: {rouge_train_average:.4f}")
 
+
+    # Test loop
+    t5_model.eval()
+    esm_model.eval()
+    projection.eval()
+
+    rouge_test_accumulated = 0.0
+    num_test_batches = 0
+
+    with torch.no_grad():
+        for batch in test_loader:
+            num_test_batches += 1
+
+            text = batch["text_list"]
+            labels = batch["labels"].to(device)
+
+            concat_hidden_states = concat_seqs(text)
+
+            projected_hidden_states = projection(concat_hidden_states)
+
+            decoder_input_ids = torch.cat((torch.full((labels.size(0), 1), 0, dtype=torch.long, device=device), labels[:, :-1]), dim=-1)
+
+            test_outputs = t5_model(
+                input_ids=None,
+                attention_mask=None,
+                decoder_input_ids=decoder_input_ids,
+                encoder_outputs=(projected_hidden_states, None),
+                labels=labels,
+            )
+
+            test_predicted_labels = t5_tokenizer.decode(test_outputs.logits[0].argmax(dim=-1).tolist(), skip_special_tokens=True)
+            test_true_labels = [batch["label"][0]]
+            test_rouge_score = rouge(test_predicted_labels, test_true_labels)["rouge1_fmeasure"]
+            rouge_test_accumulated += test_rouge_score
+            #print(f"test_true_labels: {test_true_labels}, test_predicted_labels: {test_predicted_labels}, test_rouge_score: {test_rouge_score}")
+    print(f"Epoch {epoch + 1}/{num_epochs}, Avg Train ROUGE-1 F1 Score: {rouge_train_accumulated / num_train_batches}")
+    print(f"Epoch {epoch + 1}/{num_epochs}, Avg Test ROUGE-1 F1 Score: {rouge_test_accumulated / num_test_batches}")
