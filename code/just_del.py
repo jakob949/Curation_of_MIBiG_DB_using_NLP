0f2590ab329db1d42831610c5faf8a09e96ed5dc
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-06-11 20:16:33 +0200

    MSA + entropy + pop positions

--- code/just_del.py
+++ code/just_del.py
@@ -137,6 +137,12 @@ def search_homologous_sequences(args):
 # e = time.time()
 # print(e-s, "seconds")
 
+import re
+import os
+from Bio.Align import AlignInfo
+from Bio import AlignIO
+import math
+
 def contains_gap(filename):
     with open(filename, "r") as f:
         for line in f:
@@ -154,101 +160,114 @@ def shannon_entropy(list_input):
             entropy += - p_x * math.log2(p_x)
     return entropy
 
+def process_files(start, end):
+    file_list = sorted(os.listdir("../blast/"))
+    file_list = file_list[start:end]
 
-import re
-import os
-from Bio.Align import AlignInfo
-from Bio import AlignIO
-import math
+    for ii, filename in enumerate(file_list, start=start):
 
-for ii, filename in enumerate(os.listdir("blast/"), start=131):
-    file_identifier = filename.split('_')[2:]
-    file_identifier[-1] = file_identifier[-1].split('.')[0]
-    print(ii)
-    with open(f"blast/{filename}", "r") as f:
-        query_list = []
-        subject_list = []
-        q_string, s_string = "", ""
-        for line in f:
-            if line.startswith('>'):
-                if q_string and s_string:
-                    if len(query_list) == 0:
-                        query_list.append(re.sub(r'\s', '', q_string))
-                    subject_list.append(re.sub(r'\s', '', s_string))
-                q_string, s_string = "", ""
+        file_identifier = filename.split('_')[2:]
+        file_identifier[-1] = file_identifier[-1].split('.')[0]
+        print(ii)
+        print(file_identifier)
 
-            if line.startswith('Query '):
-                query = line[10:].strip()
-                query = ''.join([i for i in query if not i.isdigit()])
-                q_string += query
+        with open(f"../blast/{filename}", "r") as f:
+            query_list = []
+            subject_list = []
+            q_string, s_string = "", ""
+            for line in f:
+                if line.startswith('>'):
+                    if q_string and s_string:
+                        if len(query_list) == 0:
+                            query_list.append(re.sub(r'\s', '', q_string))
+                        subject_list.append(re.sub(r'\s', '', s_string))
+                    q_string, s_string = "", ""
 
-            elif line.startswith('Sbjct '):
-                subject = line[10:].strip()
-                subject = ''.join([i for i in subject if not i.isdigit()])
-                s_string += subject
+                if line.startswith('Query '):
+                    query = line[10:].strip()
+                    query = ''.join([i for i in query if not i.isdigit()])
+                    q_string += query
 
-        print(f"q_string: {q_string}, s_string: {s_string}")
-        if q_string and s_string:
-            if len(query_list) == 0:
-                query_list.append(re.sub(r'\s', '', q_string))
-            subject_list.append(re.sub(r'\s', '', s_string))
-        subject_list.append(query_list[0])
-
-        with open("input_sequences.txt", "w") as f:
-            for i, seq in enumerate(subject_list):
-                f.write(f">seq_{i}\n{seq}\n")
+                elif line.startswith('Sbjct '):
+                    subject = line[10:].strip()
+                    subject = ''.join([i for i in subject if not i.isdigit()])
+                    s_string += subject
 
 
-        # Define the paths and execute the Clustal Omega command
-        file_path = os.getcwd()
-        data_path = os.getcwd()
-        file = 'input_sequences.txt'
-        os.system(f"clustalo -i {file_path}/{file} --dealign -o {data_path}/{file[:-4]}.fasta --force --threads=10")
+            if q_string and s_string:
+                if len(query_list) == 0:
+                    query_list.append(re.sub(r'\s', '', q_string))
+                subject_list.append(re.sub(r'\s', '', s_string))
 
-        # Now read the alignment after the appropriate alignment has been done
-        alignment = AlignIO.read("input_sequences.fasta", "fasta")
-        summary_align = AlignInfo.SummaryInfo(alignment)
-
-        scores = []
-        for i in range(len(alignment[0])):
-            column_bases = alignment[:, i]
-            scores.append(shannon_entropy(column_bases))
-
-        # Use the dumb_consensus method to get the consensus sequence
-        consensus = summary_align.dumb_consensus()
+            subject_list.append(query_list[0])
 
 
-        while len(consensus) > 850:
-            index = scores.index(max(scores))
-            consensus = consensus[:index] + consensus[index + 1:]
-            del scores[index]
 
-        shorten = consensus
+            with open("input_sequences.txt", "w") as f:
+                for i, seq in enumerate(subject_list):
+                    f.write(f">seq_{i}\n{seq}\n")
 
 
-        # First read all lines from the file and close it
-        with open("Transformer_DB_Curation_MIBiG/code/dataset/protein_SMILE/dataset_protein_peptides_complete_v3_shorten.txt", "r") as infile:
-            lines = infile.readlines()
 
-        # Now open the file in write mode to write back the modified lines
-        with open("Transformer_DB_Curation_MIBiG/code/dataset/protein_SMILE/dataset_protein_peptides_complete_v3_shorten.txt", "w") as outfile:
-            for i, line in enumerate(lines):
-                d1 = line.split('\t')[0]
-                smile = line.split('\t')[1]
-                data = d1.split('_')[1:]
+            # Define the paths and execute the Clustal Omega command
+            file_path = os.getcwd()
+            data_path = os.getcwd()
+            file = 'input_sequences.txt'
+            os.system(f"clustalo -i {file_path}/{file} --dealign -o {data_path}/{file[:-4]}.fasta --force --threads=10")
 
-                if i == int(file_identifier[0]):
+            # Now read the alignment after the appropriate alignment has been done
+            alignment = AlignIO.read("input_sequences.fasta", "fasta")
+            summary_align = AlignInfo.SummaryInfo(alignment)
 
-                    # Check - To insure correct file
-                    if len(data[int(file_identifier[1])]) == int(file_identifier[2]):
-                        print("replaced it")
-                        data[int(file_identifier[1])] = shorten
-                        print(type(d1), type(data))
-                        # Convert the Seq object to a string
-                        data = [str(element) for element in data]
-                        # Recreate the string for the line
-                        new_d1 = '_'.join([d1.split('_')[0]] + data)
-                        line = '\t'.join([new_d1, smile])
+            scores = []
+            for i in range(len(alignment[0])):
+                column_bases = alignment[:, i]
+                scores.append(shannon_entropy(column_bases))
 
-                outfile.write(line)
+            # Use the dumb_consensus method to get the consensus sequence
+            consensus = summary_align.dumb_consensus()
 
+
+            while len(consensus) > 850:
+                index = scores.index(max(scores))
+                consensus = consensus[:index] + consensus[index + 1:]
+                del scores[index]
+
+            shorten = consensus
+
+
+            # First read all lines from the file and close it
+            with open("Transformer_DB_Curation_MIBiG/code/dataset/protein_SMILE/dataset_protein_peptides_complete_v3_shorten.txt", "r") as infile:
+                lines = infile.readlines()
+
+            # Now open the file in write mode to write back the modified lines
+            with open("Transformer_DB_Curation_MIBiG/code/dataset/protein_SMILE/dataset_protein_peptides_complete_v3_shorten.txt", "w") as outfile:
+                for i, line in enumerate(lines):
+                    d1 = line.split('\t')[0]
+                    smile = line.split('\t')[1]
+                    data = d1.split('_')[1:]
+
+                    if i == int(file_identifier[0]):
+
+                        # Check - To insure correct file
+                        if len(data[int(file_identifier[1])]) == int(file_identifier[2]):
+                            print("replaced it")
+                            data[int(file_identifier[1])] = shorten
+                            print(type(d1), type(data))
+                            # Convert the Seq object to a string
+                            data = [str(element) for element in data]
+                            # Recreate the string for the line
+                            new_d1 = '_'.join([d1.split('_')[0]] + data)
+                            line = '\t'.join([new_d1, smile])
+
+                    outfile.write(line)
+
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("start", help="Start index for file processing", type=int)
+    parser.add_argument("end", help="End index for file processing", type=int)
+    args = parser.parse_args()
+
+    process_files(args.start, args.end)
