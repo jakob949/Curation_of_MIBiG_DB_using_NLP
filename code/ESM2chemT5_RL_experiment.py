6904aae737e64698d5bc3351aa46aa3bd7421ad9
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-06-15 14:04:39 +0200

    RL

--- code/ESM2chemT5_RL_experiment.py
+++ code/ESM2chemT5_RL_experiment.py
@@ -215,7 +215,10 @@ for epoch in range(num_epochs):
 
         # Sample from the policy to get predicted tokens
         print("Shape before reshaping:", probs.shape)
-        probs = probs.view(-1, probs.size(-1))  # reshape to (batch_size*sequence_length, num_tokens)
+        try:
+            probs = probs.view(-1, probs.size(-1))  # reshape to (batch_size*sequence_length, num_tokens)
+        except:
+            print("Shape before reshaping:", probs.shape)
 
         predicted_tokens = torch.multinomial(probs, 1)
         predicted_tokens = predicted_tokens.view(1, t5_config.d_model, -1)  # Reshape to 3D
