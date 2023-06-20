29060297dfdcb194f008550df70e3d3267a389a6
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-06-20 12:44:13 +0200

    save train

--- code/EMS2_to_T5.py
+++ code/EMS2_to_T5.py
@@ -33,8 +33,9 @@ class ProteinDataset(torch.utils.data.Dataset):
                 text_list = text.split('_')
 
                 # Check if any element in text_list is longer than 2000 characters
-                if all(len(element) <= 851 for element in text_list):
-                    data.append((text_list, label))
+                # if all(len(element) <= 851 for element in text_list):
+                #     data.append((text_list, label))
+                data.append((text_list, label))
         print(len(data))
         return data
 
