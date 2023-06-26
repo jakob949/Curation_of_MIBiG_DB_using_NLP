a4fb4ec2675eecfe6cab1d089004e1634f96a54c
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-06-26 23:19:25 +0200

    save train

--- code/T5.py
+++ code/T5.py
@@ -107,9 +107,9 @@ for epoch in range(num_epochs):
         inputs = batch["input_ids"].to(device)
         attention_mask = batch["attention_mask"].to(device)
         labels = batch["labels"].to(device)
-        print('labels: ', labels)
         num_train_batches += 1
-
+        train_true_labels = [t5_tokenizer.decode(label.tolist(), skip_special_tokens=True) for label in batch["labels"]]
+        print('true: ', train_true_labels)
         # compute the model output
         outputs = t5_model(input_ids=inputs, attention_mask=attention_mask, labels=labels)
         loss = outputs.loss
