3cd4c64a754b25531489ca0e15b9bf434b9b4740
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-07-11 23:25:38 +0200

    product

--- code/T5.py
+++ code/T5.py
@@ -100,7 +100,7 @@ t5_model.to(device)
 train_dataset = Dataset("dataset/smile_biosynclass/train_SMILE_biosyn_class_v2_0.txt", t5_tokenizer)
 test_dataset = Dataset("dataset/smile_biosynclass/test_SMILE_biosyn_class_v2_0.txt", t5_tokenizer)
 
-batch_size_train = 5
+batch_size_train = 2
 train_loader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
