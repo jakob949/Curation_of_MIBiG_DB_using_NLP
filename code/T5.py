1da5abe2dd8b5e398a47f30a82f483f3389fc860
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-09-20 18:21:35 +0200

    product

--- code/T5.py
+++ code/T5.py
@@ -106,7 +106,7 @@ t5_model.to(device)
 train_dataset = Dataset("dataset/train_i2v_activities.txt", t5_tokenizer)
 test_dataset = Dataset("dataset/test_i2v_activities.txt", t5_tokenizer)
 
-batch_size_train = 12
+batch_size_train = 6
 train_loader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
