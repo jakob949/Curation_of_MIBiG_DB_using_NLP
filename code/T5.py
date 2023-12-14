d5aa70add8d32fc9ad5f0062f7816098ec3d52a5
jakob949 <98743287+jakob949@users.noreply.github.com>
2023-12-14 11:24:23 +0100

    train

--- code/T5.py
+++ code/T5.py
@@ -123,7 +123,7 @@ train_dataset = Dataset("dataset/pfam2SMILES/train_pfam_v2.txt", t5_tokenizer)
 test_dataset = Dataset("dataset/pfam2SMILES/test_pfam_v2.txt", t5_tokenizer)
 
 num_workers = 4
-batch_size_train = 6
+batch_size_train = 4
 
 # Modify the DataLoader instances
 train_loader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True, num_workers=num_workers)
