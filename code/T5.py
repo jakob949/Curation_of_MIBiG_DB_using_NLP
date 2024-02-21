908e9193f9b2ad023000955452f7eee6423d826d
N. Jakob Larsen <98743287+jakob949@users.noreply.github.com>
2024-02-21 15:12:17 +0100

    Update T5.py

--- code/T5.py
+++ code/T5.py
@@ -3,13 +3,13 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer, AutoTokenizer, AutoModel, AdamW
 import time
-from sklearn.metrics import accuracy_score, f1_score
-from torch.optim.lr_scheduler import CosineAnnealingLR
+# from sklearn.metrics import accuracy_score, f1_score
+# from torch.optim.lr_scheduler import CosineAnnealingLR
 import argparse as arg
-from torchmetrics.text import BLEUScore, ROUGEScore
-from torchmetrics import CharErrorRate, SacreBLEUScore
-from rdkit import Chem
-# from peft import get_peft_model, LoraConfig, TaskType
+# from torchmetrics.text import BLEUScore, ROUGEScore
+# from torchmetrics import CharErrorRate, SacreBLEUScore
+# from rdkit import Chem
+from peft import get_peft_model, LoraConfig, TaskType
 
 
 def count_valid_smiles(smiles_list: list) -> int:
@@ -20,59 +20,60 @@ def count_valid_smiles(smiles_list: list) -> int:
             valid_count += 1
     return valid_count
 
+def validate_smiles(smiles_list: list) -> float:
+    loss_updata_factor = 1
+    for smiles in smiles_list:
+        mol = Chem.MolFromSmiles(smiles)
+        if mol is not None:
+            loss_updata_factor *= 0.5
+        else:
+            loss_updata_factor *= 1.75
+    return loss_updata_factor
 
 parser = arg.ArgumentParser()
 parser.add_argument("-o", "--output_file_name", type=str, default="unknown")
 parser.add_argument("-i", "--information", type=str, default="None")
 parser.add_argument("-s", "--sampling", type=bool, default=False)
+parser.add_argument("-loss", "--loss", type=bool, default=False)
+parser.add_argument("-tr", "--train", type=str, default="None")
+parser.add_argument("-te", "--test", type=str, default="None")
+parser.add_argument("-mo", "--model", type=str, default="None")
 args = parser.parse_args()
 
 
 class Dataset(Dataset):
-    def __init__(self, file_path, tokenizer, max_length=1050):
+    def __init__(self, file_path, tokenizer, max_length=2500):
         self.file_path = file_path
-        self.data = self.load_data()
         self.tokenizer = tokenizer
         self.max_length = max_length
+        self.data = self.load_data()
 
     def load_data(self):
         data = []
         num_of_truncs = 0
-        with open(self.file_path, 'r') as f:
+        excluded_seqs = 0
+        with open(self.file_path, 'r', encoding='ISO-8859-1') as f:
             for line in f:
                 text = line.split('\t')[0]
                 label = line.split('\t')[1].strip('\n')
                 text_list = text.split(': ')[1].split('_')
                 task = text.split(': ')[0]
                 if task == 'GeneName2SMILE' or task == 'pfam2SMILES':
-                    if len(text_list) < 3:
-                        num_of_truncs += 1
+                    if len(text_list) < 6:
+                        excluded_seqs += 1
                         continue
-                if task == 'ProteinSeqs2SMILE' or task == 'SMILE2Biosynclass':
-                    print('ProteinSeqs2SMILE skipped')
-                    continue
-                    # possibly implement ESM2 encoder here
+
                 if task == 'invalid2validSMILE':
                     data.append((text, label))
 
-                if len(text) > 800:
-                    data.append((text[:800], label))
+                if len(text) > self.max_length:
+                    data.append((text[:self.max_length], label))
                     num_of_truncs += 1
                 else:
                     data.append((text, label))
 
 
-                # # Check if any element in text_list is longer than 2000 characters
-                # if all(len(element) <= 800 for element in text_list):
-                #     data.append((text, label))
-                # else:
-                #     truncated_text_list = [element[:100] for element in text_list]
-                #     for item in truncated_text_list:
-                #         data.append((item, label))
-                #         if len(item) > 99:
-                #             num_of_truncs += 1
-
-        print('Num of seqs: ', len(data), 'truncated seqs: ', num_of_truncs)
+        print('Num of seqs: ', len(data), 'truncated seqs: ', num_of_truncs, 'excluded seqs: ', excluded_seqs)
         return data
 
 
@@ -90,24 +91,31 @@ class Dataset(Dataset):
             "attention_mask": input_encoding["attention_mask"].squeeze(),
             "labels": target_encoding["input_ids"].squeeze(),
             "text": text,
+            "label": label,
         }
 
 
 start_time = time.time()
 
+# Model configuration
+if args.model == "None":
+    T5_model_name = "GT4SD/multitask-text-and-chemistry-t5-base-augm"
+    t5_tokenizer = T5Tokenizer.from_pretrained("GT4SD/multitask-text-and-chemistry-t5-base-augm")
+else:
+    T5_model_name = args.model
+    try:
+        t5_tokenizer = T5Tokenizer.from_pretrained(T5_model_name)
+    except:
+        t5_tokenizer = T5Tokenizer.from_pretrained("GT4SD/multitask-text-and-chemistry-t5-base-augm")
 
-# T5_model_name = 'google/t5-efficient-tiny'
-T5_model_name = 'GT4SD/multitask-text-and-chemistry-t5-base-augm'
-# T5_model_name = 'model_020623_geneProduct2SMILES_v3.pt'
-t5_tokenizer = T5Tokenizer.from_pretrained(T5_model_name)
-# t5_model = torch.load(T5_model_name)
+t5_model = T5ForConditionalGeneration.from_pretrained(T5_model_name, torch_dtype="auto")
 
-t5_model = T5ForConditionalGeneration.from_pretrained(T5_model_name)
-# Lora peft
-# peft_config = LoraConfig(
-#     task_type=TaskType.SEQ_2_SEQ_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.3
-# )
-# t5_model = get_peft_model(t5_model, peft_config)
+# Lora peft model
+peft_config = LoraConfig(
+    task_type=TaskType.SEQ_2_SEQ_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.3
+)
+t5_model = get_peft_model(t5_model, peft_config)
+
 num_gpus = torch.cuda.device_count()
 print(f"Number of GPUs available: {num_gpus}")
 
@@ -115,52 +123,49 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 t5_model.to(device)
 t5_model = torch.nn.DataParallel(t5_model)
 
+print('Model:', type(t5_model))
+print('Devices:', t5_model.device_ids)
 
 #load data
-# dataset/train_i2v_BGC2SMM_250923.txt => no bias set
-train_dataset = Dataset("dataset/pfam2SMILES/train_pfam_v2.txt", t5_tokenizer)
-# train_dataset = Dataset("train_sampling_5_for_iv2_151223_pfam2SMILES_sampling_5.txt", t5_tokenizer)
-test_dataset = Dataset("dataset/pfam2SMILES/test_pfam_v2.txt", t5_tokenizer)
-# test_dataset = Dataset("test_sampling_5_for_iv2_151223_pfam2SMILES_sampling_5.txt", t5_tokenizer)
 
-num_workers = 10
-batch_size_train = 6
+if args.train == "None" or args.test == "None":
+    train_dataset = Dataset("dataset/Text2SMILES_Gio/i2v/train_i2v_text2smiles.txt", t5_tokenizer)
+    test_dataset = Dataset("dataset/Text2SMILES_Gio/i2v/test_i2v_text2smiles.txt", t5_tokenizer)
+else:
+    train_dataset = Dataset(args.train, t5_tokenizer)
+    test_dataset = Dataset(args.test, t5_tokenizer)
+
+batch_size_train = 15
+num_workers = 1
 
 # Modify the DataLoader instances
 train_loader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True, num_workers=num_workers)
 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=num_workers)
 
 # Optimizer
-learning_rate = 2e-4
-optimizer = AdamW(list(t5_model.parameters()), lr=learning_rate, weight_decay = 0.015)
+learning_rate = 3e-4
+optimizer = AdamW(list(t5_model.parameters()), lr=learning_rate, weight_decay = 0.0015, correct_bias = True, no_deprecation_warning = True)
 
-rouge = ROUGEScore()
-bleu = BLEUScore()
-char_error_rate = CharErrorRate()
-sacre_bleu = SacreBLEUScore()
+num_epochs = 6
 
-num_epochs = 5
 train_sampling_predictions = []
 test_sampling_predictions = []
-sampling = args.sampling
-print(sampling, "type: ", type(sampling))
-num_gen_seqs = 1
+loss = args.loss
 
+num_gen_seqs = 1
+# os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:256"
+torch.cuda.empty_cache()
 with open(f"information_{args.output_file_name}.txt", "w") as predictions_file:
-    print(">T5 model: ", T5_model_name, " Cuda available:", device, file=predictions_file)
     print(f">Learning rate: {learning_rate}, num of epoch: {num_epochs}, train batch size: {batch_size_train}", file=predictions_file)
+    print(">T5 model: ", T5_model_name, " Cuda available:", device, file=predictions_file)
     print(f">Dataset: {train_dataset.file_path}", file=predictions_file)
     print(f">Information: {args.information}", file=predictions_file)
-    # print(">peft: ", t5_model.print_trainable_parameters(), file=predictions_file)
 
 # Training loop
 for epoch in range(num_epochs):
     # print(f"Epoch {epoch + 1}/{num_epochs}")
     t5_model.train()
-    rouge_train_accumulated, bleu_train_accumulated, char_error_rate_train_accumulated, sacre_bleu_train_accumulated = 0.0, 0.0, 0.0, 0.0
     num_train_batches = 0
-    Num_correct_val_mols_train = 0
-    train_accuracy_accumulated = 0.0
 
     for batch in train_loader:
         task_train = []
@@ -170,7 +175,6 @@ for epoch in range(num_epochs):
         attention_mask = batch["attention_mask"].to(device)
         labels = batch["labels"].to(device)
         num_train_batches += 1
-        train_true_labels = [t5_tokenizer.decode(label.tolist(), skip_special_tokens=True) for label in batch["labels"]]
         outputs = t5_model(input_ids=inputs, attention_mask=attention_mask, labels=labels)
 
         if epoch == 15 and sampling:
@@ -195,47 +199,32 @@ for epoch in range(num_epochs):
                         file.write(line)
                         # print(f"iv2_sampling_{num_gen_seqs}: {generated_text}\t{true_label}")
 
-        loss = outputs.loss
-        loss = loss.mean()
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
 
-        # compute the metrics for each output
         with torch.no_grad():
-            train_predicted_labels = [t5_tokenizer.decode(logits.argmax(dim=-1).tolist(), skip_special_tokens=True) for logits in outputs.logits]
-
+            # use the generate function to get the predictions
+            generated_ids = t5_model.module.generate(input_ids=inputs, attention_mask=attention_mask, max_length=1000)
+            
+            train_predicted_labels = [t5_tokenizer.decode(generated_id, skip_special_tokens=True) for generated_id in generated_ids]
+            
+            train_true_labels = batch["label"]
+            print("train_true_labels: ", train_true_labels)
             train_true_labels = [t5_tokenizer.decode(label.tolist(), skip_special_tokens=True) for label in batch["labels"]]
-            # Num_correct_val_mols_train += count_valid_smiles(train_predicted_labels)
-
+            
             with open(f"predictions_train_{args.output_file_name}.txt", "a") as predictions_file:
                 print(f"Epoch\t{epoch + 1}\tTrue:\t{train_true_labels}\tPred:\t{train_predicted_labels}\task\t{task_train}", file=predictions_file)
 
-            train_rouge_score = rouge(train_predicted_labels, train_true_labels)["rouge1_fmeasure"]
-            train_bleu_score = bleu(train_predicted_labels, train_true_labels)
-            train_char_error_rate_score = char_error_rate(train_predicted_labels, train_true_labels)
-            train_sacre_bleu_scores = [sacre_bleu([pred], [[true]]) for pred, true in zip(train_predicted_labels, train_true_labels)]
-            train_sacre_bleu_score = sum(train_sacre_bleu_scores) / len(train_sacre_bleu_scores)
-
-            print(train_rouge_score, train_bleu_score, train_char_error_rate_score, train_sacre_bleu_score, Num_correct_val_mols_train)
-
-            batch_train_accuracy = accuracy_score(train_true_labels, train_predicted_labels)
-            train_accuracy_accumulated += batch_train_accuracy
-            rouge_train_accumulated += train_rouge_score
-            bleu_train_accumulated += train_bleu_score
-            char_error_rate_train_accumulated += train_char_error_rate_score
-            sacre_bleu_train_accumulated += train_sacre_bleu_score
-
-    # Similar loop for testing
+        loss = outputs.loss
+        if args.loss:
+            print("Alternating the loss: ", loss)
+            loss *= validate_smiles(train_predicted_labels)
+        loss.sum().backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        torch.cuda.empty_cache()
     t5_model.eval()
 
-    rouge_test_accumulated = 0.0
-    bleu_test_accumulated = 0.0
-    char_error_rate_test_accumulated = 0.0
-    sacre_bleu_test_accumulated = 0.0
     num_test_batches = 0
-    Num_correct_val_mols_test = 0
-    test_accuracy_accumulated = 0.0
+
 
     for batch in test_loader:
         task_test = []
@@ -258,55 +247,22 @@ for epoch in range(num_epochs):
             for i in range(inputs.size(0)):
                 generated_texts = [t5_tokenizer.decode(generated_id, skip_special_tokens=True)
                                    for generated_id in generated_ids[i]]
-                input_text = t5_tokenizer.decode(batch["input_ids"][i].tolist(), skip_special_tokens=True)
-                true_label = t5_tokenizer.decode(batch["labels"][i].tolist(),
-                                                 skip_special_tokens=True)  # Get the true label for the current input
+                true_label = t5_tokenizer.decode(batch["labels"][i].tolist(), skip_special_tokens=True)
 
                 # Saving predictions with corresponding true labels
                 with open(f'test_sampling_{num_gen_seqs}_for_iv2_{args.output_file_name}.txt', 'a') as file:
-                    for generated_text in generated_texts:
-                        line = f"iv2_sampling_{num_gen_seqs}: {generated_text}\t{true_label}\n"  # Pair each prediction with the true label
-                        file.write(line)
-                        # print(f"iv2_sampling_{num_gen_seqs}: {generated_text}\t{true_label}")
+                        for generated_text in generated_texts:
+                            line = f"iv2_sampling_{num_gen_seqs}: {generated_text}\t{true_label}\n"  # Pair each prediction with the true label
+                            file.write(line)
 
         with torch.no_grad():
-            outputs = t5_model(input_ids=inputs, attention_mask=attention_mask, labels=labels)
-            test_predicted_labels = [t5_tokenizer.decode(logits.argmax(dim=-1).tolist(), skip_special_tokens=True) for logits in outputs.logits]
+            # use the generate function to get the predictions
+            outputs = t5_model.module.generate(input_ids=inputs, attention_mask=attention_mask, max_length=1000)
             test_true_labels = [t5_tokenizer.decode(label.tolist(), skip_special_tokens=True) for label in batch["labels"]]
-
-            # Num_correct_val_mols_test += count_valid_smiles(test_predicted_labels)
+            generated_ids = outputs.view(inputs.size(0), num_gen_seqs, -1)
+            generated_texts = [t5_tokenizer.decode(generated_id, skip_special_tokens=True) for generated_id in generated_ids[0]]
 
             with open(f"predictions_test_{args.output_file_name}.txt", "a") as predictions_file:
-                print(f"Epoch {epoch + 1}\tTrue:\t{test_true_labels}\tPred:\t{test_predicted_labels}\ttask\t{task_test}",
-                      file=predictions_file)
+                print(f"Epoch\t{epoch + 1}\tTrue:\t{test_true_labels}\tPred:\t{generated_texts}\ttask\t{task_test}",file=predictions_file)
+    t5_model.module.save_pretrained(f"models/model_{args.output_file_name}_{epoch}.pt")
 
-            test_rouge_score = rouge(test_predicted_labels, test_true_labels)["rouge1_fmeasure"]
-            test_bleu_score = bleu(test_predicted_labels, test_true_labels)
-            test_char_error_rate_score = char_error_rate(test_predicted_labels, test_true_labels)
-            test_sacre_bleu_scores = [sacre_bleu([pred], [[true]]) for pred, true in zip(test_predicted_labels, test_true_labels)]
-            test_sacre_bleu_score = sum(test_sacre_bleu_scores) / len(test_sacre_bleu_scores)
-            batch_test_accuracy = accuracy_score(test_true_labels, test_predicted_labels)
-
-            test_accuracy_accumulated += batch_test_accuracy
-            rouge_test_accumulated += test_rouge_score
-            bleu_test_accumulated += test_bleu_score
-            char_error_rate_test_accumulated += test_char_error_rate_score
-            sacre_bleu_test_accumulated += test_sacre_bleu_score
-
-            print(test_rouge_score, test_bleu_score, test_char_error_rate_score, test_sacre_bleu_score, batch_test_accuracy, Num_correct_val_mols_test)
-
-    # Print and save results for this epoch
-    with open(f"scores_{args.output_file_name}.txt", "a") as scores_file:
-        print(
-            f"Epoch\t{epoch + 1}\tTrain Accuracy:\t{train_accuracy_accumulated / num_train_batches}\tAvg Train ROUGE-1 F1 Score\t{rouge_train_accumulated / num_train_batches}\tAvg Train BLEU Score\t{bleu_train_accumulated / num_train_batches}\tAvg Train Char Error Rate\t{char_error_rate_train_accumulated / num_train_batches}\tAvg Train SacreBLEU Score\t{sacre_bleu_train_accumulated / num_train_batches}\tNum correct val mols train\t{Num_correct_val_mols_train}",
-            file=scores_file)
-
-        print(
-            f"Epoch\t{epoch + 1}\tTest Accuracy:\t{test_accuracy_accumulated / num_test_batches}\tAvg Test ROUGE-1 F1 Score\t{rouge_test_accumulated / num_test_batches}\tAvg Test BLEU Score\t{bleu_test_accumulated / num_test_batches}\tAvg Test Char Error Rate\t{char_error_rate_test_accumulated / num_test_batches}\tAvg Test SacreBLEU Score\t{sacre_bleu_test_accumulated / num_test_batches}\tNum correct val mols test\t{Num_correct_val_mols_test}",
-            file=scores_file)
-    # save the model
-    if epoch == 4 or epoch == 3:
-
-        t5_model.save_pretrained(f"models/model_{args.output_file_name}_{epoch}.pt")
-    # torch.save(t5_model, f"models/model_{args.output_file_name}_{epoch}.pt")
-    # t5_model.module.config.to_json_file(f"models/config_{args.output_file_name}_{epoch}.json")
